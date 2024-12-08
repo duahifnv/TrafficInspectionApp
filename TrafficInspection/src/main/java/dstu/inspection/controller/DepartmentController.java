36ed63d8693ae54ccc911a877c33232d90312ec5
duahifnv <max135603@gmail.com>
2024-12-08 21:22:48 +0300

    Другая аннотация

--- TrafficInspection/src/main/java/dstu/inspection/controller/DepartmentController.java
+++ TrafficInspection/src/main/java/dstu/inspection/controller/DepartmentController.java
@@ -5,6 +5,7 @@ import dstu.inspection.service.DepartmentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
-@RestController
+@Controller
 @RequestMapping("/department")
 @RequiredArgsConstructor
 public class DepartmentController {
