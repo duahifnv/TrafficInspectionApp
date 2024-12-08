ed5bfdf00feebe9d01d28d5e770427b314c00ba4
duahifnv <max135603@gmail.com>
2024-12-09 00:20:29 +0300

    Перенос файлов

---
+++ TrafficInspection/src/main/java/dstu/inspection/entity/security/Role.java
@@ -0,0 +1,21 @@
+package dstu.inspection.entity.security;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long roleId;
+    private String name;
+}
---
+++ TrafficInspection/src/main/java/dstu/inspection/entity/security/User.java
@@ -0,0 +1,30 @@
+package dstu.inspection.entity.security;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Collection;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+    private String phone;
+    private String email;
+    private String password;
+    @Column(name = "license_id")
+    private Integer license_id;
+    @ManyToMany
+    @JoinTable(name = "users_roles",
+    joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Collection<Role> roles;
+}
