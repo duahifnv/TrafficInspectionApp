c2b8231fc87dae098982956b370ea8a46b5f502b
duahifnv <max135603@gmail.com>
2024-12-09 03:51:19 +0300

    Внедрение нового контроллера подразделений

--- TrafficInspection/src/main/java/dstu/inspection/service/InfoService.java
+++ TrafficInspection/src/main/java/dstu/inspection/service/InfoService.java
@@ -1,9 +1,11 @@
 package dstu.inspection.service;
 
+import dstu.inspection.entity.Department;
 import dstu.inspection.entity.info.CategoriesInfo;
 import dstu.inspection.entity.info.EmployeesInfo;
 import dstu.inspection.entity.info.FinesInfo;
 import dstu.inspection.repository.info.CategoriesInfoRepository;
+import dstu.inspection.repository.info.DepartmentInfoRepository;
 import dstu.inspection.repository.info.EmployeesInfoRepository;
 import dstu.inspection.repository.info.FinesInfoRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +19,7 @@ public class InfoService {
     private final CategoriesInfoRepository categoriesInfoRepository;
     private final FinesInfoRepository finesInfoRepository;
     private final EmployeesInfoRepository employeesInfoRepository;
+    private final DepartmentInfoRepository departmentInfoRepository;
     public List<CategoriesInfo> findAllCategories() {
         return categoriesInfoRepository.findAll();
     }
@@ -26,5 +29,7 @@ public class InfoService {
     public List<EmployeesInfo> findAllEmployees() {
         return employeesInfoRepository.findAll();
     }
-    // ...
+    public List<Department> findAllDepartments() {
+        return departmentInfoRepository.findAll();
+    }
 }
