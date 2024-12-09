f7b181d960ed42ac0ca25859d2256c7df3880bd4
duahifnv <max135603@gmail.com>
2024-12-09 03:52:46 +0300

    Настроена защита основных подразделов

--- TrafficInspection/src/main/java/dstu/inspection/config/SecurityConfig.java
+++ TrafficInspection/src/main/java/dstu/inspection/config/SecurityConfig.java
@@ -1,21 +1,46 @@
 package dstu.inspection.config;
 
 import dstu.inspection.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final UserService userService;
+    @Bean
+    public SecurityFilterChain adminFilterChain(HttpSecurity http) throws Exception {
+        http.securityMatcher("/**")
+                .authorizeHttpRequests(authorize ->
+                        authorize.requestMatchers("/admin/**").hasRole("ADMIN")
+                                .requestMatchers("/me/**").authenticated()
+                                .anyRequest().permitAll()
+                ).formLogin(Customizer.withDefaults())
+                .logout(authorize -> authorize.logoutSuccessUrl("/"))
+                .requestCache(cache -> cache.requestCache(requestCache()));
+        return http.build();
+    }
+    @Bean
+    public RequestCache requestCache() {
+        HttpSessionRequestCache requestCache = new HttpSessionRequestCache();
+        requestCache.setMatchingRequestParameterName(null);
+        return requestCache;
+    }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
