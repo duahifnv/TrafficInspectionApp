08c9e3719b25457781a6358e5b231adb006c4579
Максим Фоминцев <max135603@gmail.com>
2024-12-12 00:06:06 +0300

    Ренейм

--- TrafficInspection/src/main/java/dstu/inspection/controller/AuthenticationController.java
+++ TrafficInspection/src/main/java/dstu/inspection/controller/AuthenticationController.java
@@ -3,25 +3,26 @@ package dstu.inspection.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-public class LoginController {
+@RequiredArgsConstructor
+public class AuthenticationController {
     private final SecurityContextLogoutHandler logoutHandler =
             new SecurityContextLogoutHandler();
+    // Получаем страницу авторизации
     @GetMapping("/login")
-    public String loginForm() {
+    public String loginPage() {
         return "pages/login";
     }
+    // Сабмитаем выход из учетной записи
     @GetMapping("/logout")
-    public String logoutForm(Authentication authentication,
-                             HttpServletRequest request,
-                             HttpServletResponse response) {
+    public String logoutHandle(HttpServletRequest request,
+                               HttpServletResponse response,
+                               Authentication authentication) {
         logoutHandler.logout(request, response, authentication);
         return "redirect:/";
     }
