2968435d64d81329e8672f866a76418266939295
Максим Фоминцев <max135603@gmail.com>
2024-12-12 17:42:17 +0300

    Эндпоинты для добавления удостоверений и погашения нарушений

--- TrafficInspection/src/main/java/dstu/inspection/controller/UserController.java
+++ TrafficInspection/src/main/java/dstu/inspection/controller/UserController.java
@@ -1,25 +1,41 @@
 package dstu.inspection.controller;
 
+import dstu.inspection.dto.LicenseDto;
+import dstu.inspection.entity.License;
+import dstu.inspection.entity.Violation;
 import dstu.inspection.entity.info.LicensesInfo;
 import dstu.inspection.entity.info.VehiclesInfo;
 import dstu.inspection.entity.info.ViolationsInfo;
+import dstu.inspection.entity.security.User;
 import dstu.inspection.service.InfoService;
+import dstu.inspection.service.LicenseService;
+import dstu.inspection.service.UserService;
+import dstu.inspection.service.ViolationService;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.Temporal;
+import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 @RequestMapping("/me")
 @RequiredArgsConstructor
 public class UserController {
     private final InfoService infoService;
+    private final LicenseService licenseService;
+    private final UserService userService;
+    private final ViolationService violationService;
     @GetMapping
     public String profilePage() {
         return "redirect:/me/license";
@@ -29,17 +45,47 @@ public class UserController {
         return "pages/license";
     }
     @GetMapping("/license/new")
-    public String licenseForm() {
-        return "New license form";
+    public String licensePage(Model model, HttpSession session) {
+        if (session.getAttribute("license") != null) {
+            return "redirect:/me/license";
+        }
+        model.addAttribute("license", new LicenseDto());
+        return "pages/license_form";
     }
     @PostMapping("/license/new")
-    public String sendLicenseForm(@ModelAttribute LicensesInfo licensesInfo) {
-        // infoService.addLicense(licensesInfo);
-        return "redirect:/license";
+    public String sendLicenseForm(@ModelAttribute @Valid LicenseDto licenseDto,
+                                  BindingResult result, Model model,
+                                  Principal principal, HttpSession session) {
+        if (result.hasErrors()) {
+            return "pages/license_form";
+        }
+        License license = licenseService.findById(licenseDto.getLicenseId());
+        if (license == null ||
+                !Objects.equals(license.getDepartmentId(), licenseDto.getDepartmentId())) {
+            model.addAttribute("licenseError",
+                    "Не найдено удостоверения с таким кодом или кодом подразделения");
+            return "pages/license_form";
+        }
+        Long licenseId = license.getLicenseId();
+        User user = userService.findByPhone(principal.getName());
+        user.setLicenseId(licenseId);
+        userService.updateUser(user);
+        session.setAttribute("license", infoService.findLicenseInfoById(licenseId));
+        return "redirect:/me/license";
+    }
+    @PostMapping("/license/delete")
+    public String deleteLicense(Principal principal, HttpSession session) {
+        User user = userService.findByPhone(principal.getName());
+        if (user.getLicenseId() != null) {
+            user.setLicenseId(null);
+            userService.updateUser(user);
+            session.setAttribute("license", null);
+        }
+        return "redirect:/me/license";
     }
     @GetMapping("/vehicles")
     public String vehiclesTab(Model model, Principal principal) {
-        List<VehiclesInfo> userVehicles = infoService.findVehiclesByPhone(principal.getName());
+        List<VehiclesInfo> userVehicles = infoService.findVehicleInfoByPhone(principal.getName());
         model.addAttribute("vehicles",
                 userVehicles);
         return "pages/vehicles";
@@ -50,5 +96,15 @@ public class UserController {
         model.addAttribute("violations",
                 violations);
         return "pages/violations";
+    }
+    @PostMapping("/violations/{id}")
+    public String payoffViolation(@PathVariable Long id) {
+        Violation violation = violationService.findById(id);
+        if (violation == null) {
+            return "redirect:/me/violations";
+        }
+        violation.setDateOfPayment(Date.from(Instant.now()));
+        violationService.save(violation);
+        return "redirect:/me/violations";
     }
 }
