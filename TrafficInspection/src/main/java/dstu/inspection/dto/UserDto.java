a53dada630e14569366f673628c2500200cea716
Максим Фоминцев <max135603@gmail.com>
2024-12-12 04:06:26 +0300

    UserDto

---
+++ TrafficInspection/src/main/java/dstu/inspection/dto/UserDto.java
@@ -0,0 +1,21 @@
+package dstu.inspection.dto;
+
+import dstu.inspection.validation.ValidEmail;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+@Data
+public class UserDto {
+    @NotNull
+    @NotEmpty
+    private String phone;
+    @NotNull
+    @NotEmpty
+    @ValidEmail(message = "Email должен соответствовать формату")
+    private String email;
+    @NotNull
+    @NotEmpty
+    @Size(min = 6, message = "Пароль должен быть длиной минимум 6 символов")
+    private String password;
+}
