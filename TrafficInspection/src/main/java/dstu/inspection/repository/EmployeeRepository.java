72816e39b451bc05c809e71c748eb92dd5b3cddc
Максим Фоминцев <max135603@gmail.com>
2024-12-16 03:39:05 +0300

    Репо

--- TrafficInspection/src/main/java/dstu/inspection/repository/EmployeeRepository.java
+++ TrafficInspection/src/main/java/dstu/inspection/repository/EmployeeRepository.java
@@ -3,8 +3,12 @@ package dstu.inspection.repository;
 import dstu.inspection.entity.Driver;
 import dstu.inspection.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    @Query("select emp from Employee emp inner join User as u " +
+            "on u.password = emp.accessKey where u.username = ?1")
+    Employee findByUsername(String username);
 }
--- TrafficInspection/src/main/java/dstu/inspection/repository/info/ViolationsInfoRepository.java
+++ TrafficInspection/src/main/java/dstu/inspection/repository/info/ViolationsInfoRepository.java
@@ -2,6 +2,7 @@ package dstu.inspection.repository.info;
 
 import dstu.inspection.entity.info.CategoriesInfo;
 import dstu.inspection.entity.info.ViolationsInfo;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -10,6 +11,8 @@ import java.util.List;
 
 @Repository
 public interface ViolationsInfoRepository extends JpaRepository<ViolationsInfo, Long> {
+    @Override
+    List<ViolationsInfo> findAll(Sort sort);
     List<ViolationsInfo> findViolationsByRegistrationCode(String registrationCode);
     @Query("select info from ViolationsInfo info " +
             "inner join UsersLicensesInfo u on info.registrationCode = u.registrationCode " +
