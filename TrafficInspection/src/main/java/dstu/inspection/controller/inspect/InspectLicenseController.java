4b162a1e12c5803ec1ead62d268d80cc72a6385c
Максим Фоминцев <max135603@gmail.com>
2024-12-20 15:44:36 +0300

    Эндпоинты для изменения удостоверений

--- TrafficInspection/src/main/java/dstu/inspection/controller/inspect/InspectLicenseController.java
+++ TrafficInspection/src/main/java/dstu/inspection/controller/inspect/InspectLicenseController.java
@@ -1,7 +1,12 @@
 package dstu.inspection.controller.inspect;
 
+import dstu.inspection.dto.ViolationDto;
 import dstu.inspection.dto.inspect.LicenseDto;
 import dstu.inspection.entity.License;
+import dstu.inspection.entity.Violation;
+import dstu.inspection.mapper.LicenseMapper;
+import dstu.inspection.service.DepartmentService;
+import dstu.inspection.service.DriverService;
 import dstu.inspection.service.InfoService;
 import dstu.inspection.service.LicenseService;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +16,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -25,19 +31,32 @@ import java.util.Date;
 public class InspectLicenseController {
     private final LicenseService licenseService;
     private final InfoService infoService;
+    private final DriverService driverService;
+    private final LicenseMapper licenseMapper;
     @GetMapping("/licenses")
     public String licensesPage(Model model) {
         model.addAttribute("licenses", infoService.findAllLicenseInfos());
         return "pages/employee/all_licenses";
     }
     @GetMapping("/licenses/new")
-    public String newLicenseForm(LicenseDto licenseDto) {
+    public String newLicenseForm(LicenseDto licenseDto, Model model) {
+        addStaticAttributes(model);
         return "pages/employee/new_license_form";
     }
     @PostMapping("/licenses/new")
     public String putLicenseForm(@Validated LicenseDto licenseDto,
-                                  BindingResult result) throws ParseException {
+                                  BindingResult result, Model model) throws ParseException {
         if (result.hasErrors()) {
+            addStaticAttributes(model);
+            return "pages/employee/new_license_form";
+        }
+        License licenseByDriverId = licenseService.findByDriverId(licenseDto.getDriverId());
+        if (licenseByDriverId != null) {
+            String errorMessage = "Этому водителю уже выдано удостоверение";
+            FieldError error = new FieldError("licenseDto",
+                    "driverId", errorMessage);
+            result.addError(error);
+            addStaticAttributes(model);
             return "pages/employee/new_license_form";
         }
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
@@ -51,5 +70,38 @@ public class InspectLicenseController {
                 .build();
         licenseService.save(license);
         return "redirect:/inspect/licenses";
+    }
+    @GetMapping("/licenses/edit/{id}")
+    public String licenseEditForm(@PathVariable Long id, Model model) {
+        License license = licenseService.findById(id);
+        if (license == null) {
+            return "redirect:/inspect/licenses";
+        }
+        LicenseDto licenseDto = licenseMapper.modelToDto(license);
+        addStaticAttributes(model);
+        model.addAttribute("licenseDto", licenseDto);
+        return "pages/employee/edit_license_form";
+    }
+    @PostMapping("/licenses/edit/{id}")
+    public String sendViolationEditForm(@Validated LicenseDto licenseDto, BindingResult result,
+                                        @PathVariable Long id, Model model) {
+        if (result.hasErrors()) {
+            addStaticAttributes(model);
+            return "pages/employee/edit_license_form";
+        }
+        License license = licenseMapper.dtoToModel(licenseDto);
+        licenseService.save(license);
+        return "redirect:/inspect/licenses";
+    }
+    @PostMapping("/licenses/{id}")
+    public String deleteViolation(@PathVariable Long id) {
+        if (licenseService.findById(id) != null) {
+            licenseService.deleteById(id);
+        }
+        return "redirect:/inspect/licenses";
+    }
+    private void addStaticAttributes(Model model) {
+        model.addAttribute("driverList", driverService.findAll());
+        model.addAttribute("departmentList", infoService.findAllDepartments());
     }
 }
