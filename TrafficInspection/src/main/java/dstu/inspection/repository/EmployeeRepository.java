d6ef47877590284cec4543c21a5b4d62462c9744
Максим Фоминцев <max135603@gmail.com>
2025-02-08 03:09:14 +0300

    Документация

--- TrafficInspection/src/main/java/dstu/inspection/repository/EmployeeRepository.java
+++ TrafficInspection/src/main/java/dstu/inspection/repository/EmployeeRepository.java
@@ -1,6 +1,5 @@
 package dstu.inspection.repository;
 
-import dstu.inspection.entity.Driver;
 import dstu.inspection.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,6 +7,11 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    /**
+     * Поиск сотрудника по его имени в системе
+     * @param username имя под которым сотрудник зарегистрирован в системе
+     * @return объект сотрудника
+     */
     @Query("select emp from Employee emp inner join User as u " +
             "on u.password = emp.accessKey where u.username = ?1")
     Employee findByUsername(String username);
